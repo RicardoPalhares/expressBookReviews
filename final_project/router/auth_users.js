1570ba685e298daf80ac436b0305efe824b1ce8e
Ricardo Palhares <81572237+RicardoPalhares@users.noreply.github.com>
2024-12-10 14:10:15 -0300

    Implementado 8 e 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -58,7 +58,37 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const review = req.body.review
+  const bookFilter = books[isbn]
+
+
+
+  if(bookFilter){
+    bookFilter.review = review;
+
+    return res.status(200).json({"Message ": "Update success!"});
+  }else{
+
+    return res.status(400).json({"Message ": "Don't find book!"});
+  }
+
+
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn
+  const bookFilter = books[isbn]
+
+  if(bookFilter){
+    bookFilter.review = '';
+
+    return res.status(200).json({"Message ": "Delete success!"});
+  }else{
+
+    return res.status(400).json({"Message ": "Don't find book!"});
+  }
+
 });
 
 module.exports.authenticated = regd_users;
